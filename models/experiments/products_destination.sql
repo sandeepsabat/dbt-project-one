b0cdaf4c440a508e4f1ab251b13cf37c48176b75
SandeepSabat <sandeep.sabat@gmail.com>
2024-05-30 23:12:13 +0530

    modified the product price and product cost data type and also added currency for price and cost

--- models/experiments/products_destination.sql
+++ models/experiments/products_destination.sql
@@ -1,8 +1,9 @@
 {{ config(materialized='table') }}
 
 with products_destination as (
-    select CAST(src.product_id as INTEGER),src.product_cost,src.product_name,
-    src.product_price,src.product_category
+    select CAST(src.product_id as INTEGER),src.product_name,src.product_category,
+    CAST(SUBSTRING(src.product_price FROM 2) as NUMERIC(10,2)) as product_price,SUBSTRING(src.product_price, 1,1) as product_price_currency,
+    CAST(SUBSTRING(src.product_cost FROM 2) as NUMERIC(10,2)) as product_cost, SUBSTRING(src.product_cost, 1,1) as product_cost_currenct
     from file_source_schema.srcproducts src
 )
 
